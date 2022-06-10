bc3365901a43707f1a862b794f2ccfe846f41807
Joel Coutinho <joelsteve97@hotmail.com>
2022-06-11 01:14:21 +0530

    adds tailwind

---
+++ src/pages/components/Home/index.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">Home Page</h1>
+        <br/>
+        <div className="bg-slate-500 shadow-sm container text-center ">
+            Some more text
+        </div>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { Home } from './components/Home'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Main: NextPage = () => {
+  return (
+    <Home></Home>
+  )
+}
+
+export default Main

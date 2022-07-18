959c8f7aa2d2ea090ddca029923fc23b060ae55f
jscyo <joelsteve97@hotmail.com>
2022-07-18 23:03:16 +0530

    update button text

--- src/pages/components/Home/index.tsx
+++ src/pages/components/Home/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           id="generate-url"
           onClick={generateUrlMapping}
         >
-          Button
+          Shorten!!!
         </button>
         <div className="select-all" id="linkField">
           {mappedUrl}

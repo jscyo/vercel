c459bcfaf83b4339003ae7c5c21c2d843c4d0863
Joel Coutinho <joelsteve97@hotmail.com>
2023-02-27 13:31:47 +0530

    setting up SuperTokens auth

---
+++ src/config/appInfo.ts
@@ -0,0 +1,10 @@
+
+export const appInfo = {
+    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+    appName: "link-test",
+    apiDomain: "https://vercel-jscyo.vercel.app",
+    websiteDomain: "https://vercel-jscyo.vercel.app",
+    apiBasePath: "/api/auth",
+    websiteBasePath: "/auth"
+  }
+  
